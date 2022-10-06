6818c123eddecbc202a9458966b8e230b04ae26c
Mohamed Messaoudi <mohamed.messaoudi@student.ehb.be>
2022-10-06 16:01:58 +0200

    Excercise space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,28 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+drawline();
+
+window.onresize = drawline; 
+
+function drawline() {
+
+let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   //rec
+context.fillStyle = "green";
+context.fillRect(500, 50, 200, 200);
+
+
+//rec2
+context.fillRect(300, 200, 600,200);
+
+
+
+//rec3
+context.fillRect( 300, 600, 600, 200);
+
+  } 
